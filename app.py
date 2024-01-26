02dd8f972a38ee63aaa654d7c0f7939345300999
Sridhar D Kedlaya <sridhardked@gmail.com>
2024-01-26 12:26:48 +0530

    Change admin cookie properly

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def getcookie():
 @app.route("/", methods=['GET', 'HEAD'])
 def hello_rvce():
     if request.method == 'GET':
-        if getcookie()=="YWRtaW46MQo=":
+        if getcookie()=="YWRtaW46MQ==":
             return render_template('admin.html', jobs=JOBS, company_name='RVCE')
         else:
             rendered = render_template('home.html',
